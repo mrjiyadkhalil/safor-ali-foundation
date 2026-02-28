78a0f3ed43aab2fa16cae44a8e58f7cd0710fc1b
mrjiyadkhalil <mrjiyadkhalil@gmail.com>
2026-02-28 19:24:21 +0600

    Update on

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -44,7 +44,7 @@ export default function AboutPage() {
           <h2 className="text-3xl font-bold text-center text-gray-900 mb-12">
             Executive Committee
           </h2>
-          <h3 className="text-3xl font-bold text-center text-gray-900 mb-12">
+          <h3 className="text-2xl font-bold text-center text-gray-900 mb-12">
             2025-2026
           </h3>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
